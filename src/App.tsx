06f9e823fbc109f702a7d1ff54c8ffa9954a83a2
yusuf umar faruq <luwaservices@gmail.com>
2023-06-21 11:34:03 -0700

    tailwind theme config

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <h1 className="font-inter text-secondary-700">This is e-commerce</h1>
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+      colors: {
+        primary: {
+          500: '#FFFFFF',
+          600: '#363738'
+        },
+        secondary: {
+          500: '#F5F5F5',
+          600: '#FEFAF1',
+          700: '#DB4444'
+        },
+        textColor: {
+          400: '#FAFAFA',
+          500: '#7D8184',
+          600: '#000000'
+        },
+        buttonColor: {
+          400: '#000000',
+          500: '#00FF66',
+          600: '#DB4444',
+          700: '#E07575',
+          800: '#A0BCE0'
+        }
+      },
+
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
