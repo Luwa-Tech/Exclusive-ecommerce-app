ee71797d594366fac5d21582efeb95090385d052
yusuf umar faruq <luwaservices@gmail.com>
2023-06-22 18:16:47 -0700

    mobile navbar fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,12 +11,12 @@ const NavBar = () => {
     const isDesktop = useMediaQuery({ minWidth: 640 })
     return (
         <>
-        <nav className="flex justify-between items-center w-[90%] m-auto max-w-[1200px] pt-[1rem]">
-            <section>
-                <RxTextAlignLeft className="md:hidden"/>
+        <nav className="flex justify-between items-center md:w-[90%] md:m-auto md:max-w-[1200px] pt-2 px-2 md:pt-[1rem]">
+            <section className="flex items-center gap-2">
+                <RxTextAlignLeft className="md:hidden w-[1.6rem] h-[1.6rem]"/>
                 <h1 className="text-[1.5rem] font-bold tracking-[0.045rem] text-textColor-600 leading-6">Exclusive</h1>
             </section>
-            <section className="flex column md:row gap-[3rem] text-textColor-600 ">
+            <section className="hidden md:flex md:column md:row md:gap-[3rem] text-textColor-600 ">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/contact">Contact</NavLink>
                 <NavLink to="/about">About</NavLink>
@@ -24,8 +24,8 @@ const NavBar = () => {
             <section className="flex items-center gap-[1.5rem]">
                 {isDesktop && <SearchBar/>}
                 <div className="flex items-center gap-[0.45rem]">
-                    <CiHeart className="w-[1.5rem] h-[1.5rem]"/>
-                    <CiShoppingCart className="w-[1.5rem] h-[1.5rem]"/>
+                    <CiHeart className="w-[1.5rem] h-[1.5rem] text-buttonColor-400"/>
+                    <CiShoppingCart className="w-[1.5rem] h-[1.5rem] text-buttonColor-400"/>
                 </div>
             </section>
         </nav>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,10 +2,12 @@ import { CiSearch } from "react-icons/ci"
 
 const SearchBar = () => {
     return (
-        <section className="flex column justify-center items-center gap-[2.125rem] bg-secondary-500 pt-[0.4375rem] pr-[0.75rem] pb-[0.4375rem] pl-[1.25rem] rounded-[5px]">
-            <input className="text-[0.75rem] leading-[1.125rem] bg-transparent px-[.75rem]" type="text" placeholder="What are you looking for?"/>
-            <CiSearch />
+        <div className="mt-2 md:mt-0 px-2 md:px-0"> 
+        <section className="flex items-center justify-between px-[0.4rem] py-2 md:column w-100% md:items-center md:gap-[2.125rem] bg-secondary-500 md:pt-[0.4375rem] md:pr-[0.75rem] md:pb-[0.4375rem] md:pl-[1.25rem] rounded-[5px]">
+            <input className="text-[.95rem] leading-[1.125rem] bg-transparent px-[.75rem]" type="text" placeholder="What are you looking for?"/>
+            <CiSearch  className="w-[1.4rem] h-[1.4rem] md:w-[1.1rem] md:h-[1.1rem] text-buttonColor-400"/>
         </section>
+        </div> 
     )
 }
 
